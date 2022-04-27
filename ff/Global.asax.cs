b89e09ceebd420f603156977a3612871da89dce1
yac-mtl <larkemyacine21@gmail.com>
2022-04-27 07:14:24 -0400

    ajouter system.runtime fixer les erreurs

--- ff/Global.asax.cs
+++ ff/Global.asax.cs
@@ -1,21 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+
+
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
-using Amazon.OpsWorks.Model;
-using Microsoft.AspNetCore.Mvc;
-using ServiceStack.Text;
-using Stripe;
-using Stripe.BillingPortal;
-using Stripe.Checkout;
-using HttpPostAttribute = Microsoft.AspNetCore.Mvc.HttpPostAttribute;
-using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
-using Session = Stripe.Checkout.Session;
-using SessionCreateOptions = Stripe.Checkout.SessionCreateOptions;
-using SessionService = Stripe.Checkout.SessionService;
 
 namespace ff
 {
